32dd7b45a69d388d74976526f8ab17c56e39ff16
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2024-09-12 15:18:21 +0100

    feat: sumbission for part4: blog list steps 1 and 2
    - refactore project structure to respect Node.js best parctice

---
+++ app.js
@@ -0,0 +1,20 @@
+const config = require('./utils/config')
+const blogRouter = require('./controllers/blog')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+const mongoUrl = config.MONGODB_URI
+mongoose.connect(mongoUrl)
+  .then(() => logger.info('connected to MongoDB'))
+  .catch(error => logger.error('error connecting to MongoDB', error.message))
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogRouter)
+
+module.exports = app
+
